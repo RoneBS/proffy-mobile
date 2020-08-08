645552c44f86de6397440b2c2e53305ed89f96a8
Rone Silveira <ronebs10@hotmail.com>
2020-08-08 09:29:08 -0300

    teachers filter completed

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
@@ -9,9 +11,25 @@ import styles from './styles';
 
 
 function TeacherList() {
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    function handleToggleFiltersVisible() {
+        setIsFiltersVisible(!isFiltersVisible);
+    }
+
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis">
+            <PageHeader
+                title="Proffys disponíveis"
+                headerRight={(
+                    <BorderlessButton onPress={handleToggleFiltersVisible}>
+                        <Feather name="filter" size={20} color="#FFF" />
+                    </BorderlessButton>
+                )}
+                    
+            
+            >
+                {isFiltersVisible && (
                 <View style={styles.searchForm}>
                     <Text style={styles.label}>Matemática</Text>
                     <TextInput
@@ -39,24 +57,30 @@ function TeacherList() {
                             />
                         </View>
                     </View>
+
+                    <RectButton style={styles.submitButton}>
+                        <Text style={styles.submitButtonText}>Filtrar</Text>
+                    </RectButton>
+
                 </View>
+            )}
             </PageHeader>
 
-            <ScrollView
-                style={styles.teacherList}
-                contentContainerStyle={{
-                    paddingHorizontal: 16,
-                    paddingBottom: 16,
-                }}
-            >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-            </ScrollView>
-        </View>
+        <ScrollView
+            style={styles.teacherList}
+            contentContainerStyle={{
+                paddingHorizontal: 16,
+                paddingBottom: 16,
+            }}
+        >
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+            <TeacherItem />
+        </ScrollView>
+        </View >
     );
 }
 
